c0f9ad8e805642fab834a941908f81eced0d621e
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-03-01 14:21:54 +0200

    save

---
+++ src/page/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+  return (
+    <>
+      <div>
+        <h1>
+          My Contacts - welcome page{' '}
+          <span role="img" aria-label="Greeting icon">
+            💁‍♀️
+          </span>
+        </h1>
+      </div>
+    </>
+  );
+}
