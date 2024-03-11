29796abc63da68873fa3ea757e80a49a87336ad6
Vlad Liashenko <107635535+Vladyslav-Liashenko@users.noreply.github.com>
2024-03-11 10:41:45 +0200

    save

--- src/components/DocumentTitle.jsx
+++ src/components/DocumentTitle.jsx
@@ -1,9 +1,9 @@
-import { Helmet } from 'react-helmet-async';
+// import { Helmet } from 'react-helmet-async';
 
 export default function DocumentTitle({ children }) {
   return (
-    <Helmet>
-      {/* <title>{children}</title> */}
-    </Helmet>
+    <h2>
+      <title>{children}</title>
+    </h2>
   );
 }
